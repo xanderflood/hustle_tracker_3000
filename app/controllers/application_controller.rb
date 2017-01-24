aa7f103a2f1be4171a615fe16b6fa72eae2d5425
xander flood <xander.t.flood@gmail.com>
2017-01-24 14:48:12 -0500

    Configure controllers for Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :authenticate_user_from_token!, unless: :devise_controller?
+  before_filter :authenticate_user!, unless: :devise_controller?
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  skip_before_filter :authenticate_user!
+
   # GET /users
   # GET /users.json
   def index
